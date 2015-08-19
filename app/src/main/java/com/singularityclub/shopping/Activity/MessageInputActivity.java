ede9456076cb9909dbc47e57b4f2e8ee2780184c
fenghao <593626601@qq.com>
2015-08-19 15:54:34 +0800

    init

---
+++ app/src/main/java/com/singularityclub/shopping/Activity/MessageInputActivity.java
@@ -0,0 +1,20 @@
+package com.singularityclub.shopping.Activity;
+
+import android.app.Activity;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.singularityclub.shopping.R;
+
+
+public class MessageInputActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_message_input);
+    }
+
+}
---
+++ app/src/main/java/com/singularityclub/shopping/Activity/ShowProductionActivity.java
@@ -0,0 +1,9 @@
+package com.singularityclub.shopping.Activity;
+
+import android.app.Activity;
+
+/**
+ * Created by fenghao on 2015/8/19.
+ */
+public class ShowProductionActivity extends Activity{
+}
