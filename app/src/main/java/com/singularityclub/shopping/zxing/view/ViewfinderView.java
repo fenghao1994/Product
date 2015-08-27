f161fb2452c9f6026fe5b042fd7a9a5b1bfb5d9d
fenghao <593626601@qq.com>
2015-08-27 10:21:48 +0800

    二维码效果

--- app/src/main/java/com/singularityclub/shopping/zxing/view/ViewfinderView.java
+++ app/src/main/java/com/singularityclub/shopping/zxing/view/ViewfinderView.java
@@ -25,6 +25,8 @@ import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.GradientDrawable;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -53,13 +55,18 @@ public final class ViewfinderView extends View {
   private int scannerAlpha;
   private Collection<ResultPoint> possibleResultPoints;
   private Collection<ResultPoint> lastPossibleResultPoints;
-
+  private static final int CURRENT_POINT_OPACITY = 0xA0;
+  private int i = 0;// 添加的
+  private Rect mRect;// 扫描线填充边界
+  private GradientDrawable mDrawable;// 采用渐变图作为扫描线
+  private Drawable lineDrawable;// 采用图片作为扫描线
   // This constructor is used when the class is built from an XML resource.
   public ViewfinderView(Context context, AttributeSet attrs) {
     super(context, attrs);
 
     // Initialize these once for performance rather than calling them every time in onDraw().
     paint = new Paint();
+    mRect = new Rect();
     Resources resources = getResources();
     maskColor = resources.getColor(R.color.viewfinder_mask);
     resultColor = resources.getColor(R.color.result_view);
@@ -68,6 +75,14 @@ public final class ViewfinderView extends View {
     resultPointColor = resources.getColor(R.color.possible_result_points);
     scannerAlpha = 0;
     possibleResultPoints = new HashSet<ResultPoint>(5);
+    int left = getResources().getColor(R.color.lightgreen);
+    int center = getResources().getColor(R.color.green);
+    int right = getResources().getColor(R.color.lightgreen);
+    lineDrawable = getResources().getDrawable(R.drawable.zx_code_line);
+
+    mDrawable = new GradientDrawable(
+            GradientDrawable.Orientation.LEFT_RIGHT, new int[] { left,
+            left, center, right, right });
   }
 
   @Override
@@ -87,7 +102,84 @@ public final class ViewfinderView extends View {
     canvas.drawRect(0, frame.bottom + 1, width, height, paint);
 
     if (resultBitmap != null) {
-      // Draw the opaque result bitmap over the scanning rectangle
+      // 在扫描框中画出预览图
+      paint.setAlpha(CURRENT_POINT_OPACITY);
+      canvas.drawBitmap(resultBitmap, null, frame, paint);
+    } else {
+      // 画出四个角
+      paint.setColor(getResources().getColor(R.color.green));
+      // 左上角
+      canvas.drawRect(frame.left, frame.top, frame.left + 30,
+              frame.top + 10, paint);
+      canvas.drawRect(frame.left, frame.top, frame.left + 10,
+              frame.top + 30, paint);
+      // 右上角
+      canvas.drawRect(frame.right - 30, frame.top, frame.right,
+              frame.top + 10, paint);
+      canvas.drawRect(frame.right - 10, frame.top, frame.right,
+              frame.top + 30, paint);
+      // 左下角
+      canvas.drawRect(frame.left, frame.bottom - 10, frame.left + 30,
+              frame.bottom, paint);
+      canvas.drawRect(frame.left, frame.bottom - 30, frame.left + 10,
+              frame.bottom, paint);
+      // 右下角
+      canvas.drawRect(frame.right - 30, frame.bottom - 10, frame.right,
+              frame.bottom, paint);
+      canvas.drawRect(frame.right - 10, frame.bottom - 30, frame.right,
+              frame.bottom, paint);
+
+      // 在扫描框中画出模拟扫描的线条
+      // 设置扫描线条颜色为绿色
+      paint.setColor(getResources().getColor(R.color.green));
+      // 设置绿色线条的透明值
+      paint.setAlpha(SCANNER_ALPHA[scannerAlpha]);
+      // 透明度变化
+      scannerAlpha = (scannerAlpha + 1) % SCANNER_ALPHA.length;
+
+      // 画出固定在中部的线条
+      // int middle = frame.height() / 2 + frame.top;
+      // canvas.drawRect(frame.left + 2, middle - 1, frame.right - 1,
+      // middle + 2, paint);
+
+      // 将扫描线修改为上下走的线
+      if ((i += 5) < frame.bottom - frame.top) {
+				/* 以下为用渐变线条作为扫描线 */
+        // 渐变图为矩形
+        // mDrawable.setShape(GradientDrawable.RECTANGLE);
+        // 渐变图为线型
+        // mDrawable.setGradientType(GradientDrawable.LINEAR_GRADIENT);
+        // 线型矩形的四个圆角半径
+        // mDrawable
+        // .setCornerRadii(new float[] { 8, 8, 8, 8, 8, 8, 8, 8 });
+        // 位置边界
+        // mRect.set(frame.left + 10, frame.top + i, frame.right - 10,
+        // frame.top + 1 + i);
+        // 设置渐变图填充边界
+        // mDrawable.setBounds(mRect);
+        // 画出渐变线条
+        // mDrawable.draw(canvas);
+
+				/* 以下为图片作为扫描线 */
+        mRect.set(frame.left - 6, frame.top + i - 6, frame.right + 6,
+                frame.top + 6 + i);
+        lineDrawable.setBounds(mRect);
+        lineDrawable.draw(canvas);
+
+        // 刷新
+        invalidate();
+      } else {
+        i = 0;
+      }
+
+      // 重复执行扫描框区域绘制(画四个角及扫描线)
+      postInvalidateDelayed(ANIMATION_DELAY, frame.left, frame.top,
+              frame.right, frame.bottom);
+    }
+
+
+
+     /* // Draw the opaque result bitmap over the scanning rectangle
       paint.setAlpha(OPAQUE);
       canvas.drawBitmap(resultBitmap, frame.left, frame.top, paint);
     } else {
@@ -130,7 +222,7 @@ public final class ViewfinderView extends View {
       // Request another update at the animation interval, but only repaint the laser line,
       // not the entire viewfinder mask.
       postInvalidateDelayed(ANIMATION_DELAY, frame.left, frame.top, frame.right, frame.bottom);
-    }
+*/
   }
 
   public void drawViewfinder() {
